21b3866bc1358723cd765da173e2f249b52b8f4d
shruti-chepuri <61222184+shruti-chepuri@users.noreply.github.com>
2020-04-07 00:23:32 +0530

    Add files via upload

---
+++ codes/eigen.py
@@ -0,0 +1,10 @@
+import numpy as np
+m = np.matrix([[0, 1, 0],
+               [0, 0, 1],
+               [-1, -2, 3]])
+
+eigenvalues = np.linalg.eigvals(m)
+print('The eigenvalues are:')
+print eigenvalues
+print("Roots of the the polynomial:")
+print(np.roots([1, 3, 2,1]))
